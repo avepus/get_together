4a227aef8891e08320fd6da64737302fb84ffbf1
avepus <avepus@gmail.com>
2024-01-13 22:52:23 -0600

    made into a class as it should have been

--- lib/widgets/image_with_null_error_handling.dart
+++ lib/widgets/image_with_null_error_handling.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 
-Widget ImageWithNullAndErrorHandling(String? imageUrl) {
-  return imageUrl != null
-      ? Image.network(
-          imageUrl,
-          errorBuilder: (context, error, stackTrace) {
-            return const Icon(Icons.image_not_supported);
-          },
-        )
-      : const Icon(Icons.account_circle);
+class ImageWithNullAndErrorHandling extends StatelessWidget {
+  final String? imageUrl;
+
+  const ImageWithNullAndErrorHandling({this.imageUrl, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return imageUrl != null
+        ? Image.network(
+            imageUrl!,
+            errorBuilder: (context, error, stackTrace) {
+              return const Icon(Icons.image_not_supported);
+            },
+          )
+        : const Icon(Icons.account_circle);
+  }
 }
