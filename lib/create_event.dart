379fb65870a1eabf5b21dde3ff6b7fe162e62290
avepus <avepus@gmail.com>
2024-04-16 22:42:00 -0500

    created draft of function and added tests

--- lib/create_event.dart
+++ lib/create_event.dart
@@ -1,6 +1,9 @@
 import 'availability.dart';
 import 'event.dart';
 
+///TODO: create a version of the function that prioritizes time slots with the most users available rather than the "best" availability
+///TODO: create a "smoothing" function to expand a converaged availability with nearby timeslots and take the lowest availability score
+///TODO: handle edge cases where the best availability spans a very long period of time. For now, there will be no maximum
 /// Finds time slots based on user availabilities.
 ///
 /// The function takes a map of user availabilities and returns a list of events representing the time slots.
@@ -8,7 +11,8 @@ import 'event.dart';
 /// [userAvailabilities] A map of user availabilities. The keys are user IDs.
 ///
 /// Returns a list of events representing the time slots.
-List findTimeSlots(Map<String, Availability> userAvailabilities) {
+List findTimeSlots(
+    Map<String, Availability> userAvailabilities, int timeSlotDuration) {
   List<Event> events = [];
   //TOTO; do some calculations on availability to generate events
   return events;
@@ -25,20 +29,16 @@ List<int> convergeAvailabilities(List<Availability> availabilities) {
   return converge;
 }
 
-List findNonZeroTimeSlots(List<int> convergedAvailability) {
-  List<int> bestTimes = [];
-  int bestTime = 0;
-  int bestTimeLength = 0;
-  int currentTime = 0;
-  int currentTimeLength = 0;
+List<int> calculateTimeSlotScores(
+    List<int> convergedAvailability, int timeSlotDuration) {
+  List<int> timeSlotScores = List<int>.filled(convergedAvailability.length, 0);
   for (int i = 0; i < convergedAvailability.length; i++) {
-    if (convergedAvailability[i] <= 0) {
-      continue;
+    for (int j = i; j < i + timeSlotDuration; j++) {
+      int index = j %
+          convergedAvailability
+              .length; //note we need to wrap around if availability at the end of the week continues into the next week morning
+      timeSlotScores[i] += convergedAvailability[index];
     }
   }
-  bestTimes.add(bestTime);
-  bestTimes.add(bestTime + bestTimeLength);
-  return bestTimes;
+  return timeSlotScores;
 }
-
-///TODO: create a version of the function that prioritizes time slots with the most users available rather than the "best" availability
--- test/event_creation_test.dart
+++ test/event_creation_test.dart
@@ -1,11 +1,109 @@
-
 ///need to test event creation
 ///
 ///example:
 /// 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,12,13,14,15
-/// 
+///
 ///[0, 0, 1, 0, 0, 0, 3, 3, 3, 3, 3, 3, 1, 1, 0, 0]
 ///top time should be indices 6-11
 ///bottom times should be indices 2 and 12-13
 ///attempting 4 times should only return 3
 ///having a two timeslot minimum should not return 2
+///
+///
+import 'package:test/test.dart';
+import 'package:get_together/create_event.dart';
+
+void main() {
+  test(
+      'calculateTimeSlotScores should return correct values base case timeSlotDuration = 1',
+      () {
+    int timeSlotDuration = 1;
+    List<int> convergedAvailability = [
+      -6,
+      -3,
+      1,
+      0,
+      0,
+      0,
+      3,
+      3,
+      3,
+      3,
+      3,
+      3,
+      1,
+      1,
+      0,
+      0
+    ];
+    List<int> expectedScores = [
+      -6,
+      -3,
+      1,
+      0,
+      0,
+      0,
+      3,
+      3,
+      3,
+      3,
+      3,
+      3,
+      1,
+      1,
+      0,
+      0
+    ];
+
+    List<int> actualScores =
+        calculateTimeSlotScores(convergedAvailability, timeSlotDuration);
+
+    expect(actualScores, expectedScores);
+  });
+  test(
+      'calculateTimeSlotScores should return correct values timeSlotDuration = 4',
+      () {
+    int timeSlotDuration = 4;
+    List<int> convergedAvailability = [
+      -6,
+      -3,
+      1,
+      0,
+      0,
+      0,
+      3,
+      3,
+      3,
+      3,
+      3,
+      3,
+      1,
+      1,
+      0,
+      0
+    ];
+    List<int> expectedScores = [
+      -8,
+      -2,
+      1,
+      3,
+      6,
+      9,
+      12,
+      12,
+      12,
+      10,
+      8,
+      5,
+      2,
+      -5,
+      -9,
+      -8
+    ];
+
+    List<int> actualScores =
+        calculateTimeSlotScores(convergedAvailability, timeSlotDuration);
+
+    expect(actualScores, expectedScores);
+  });
+}
