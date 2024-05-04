2c2f2685a67bf0f45c53fbf1e869c6236b4f7c5d
avepus <avepus@gmail.com>
2024-05-04 14:17:16 -0500

    align date and start time fields and add end date and time

--- lib/create_event.dart
+++ lib/create_event.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:syncfusion_flutter_datepicker/datepicker.dart';
 import 'package:intl/intl.dart';
 import 'classes/group.dart';
@@ -18,7 +19,11 @@ class CreateEventPage extends StatefulWidget {
 class _CreateEventPageState extends State<CreateEventPage> {
   final _startTimeController = TextEditingController();
   final _startDateController = TextEditingController();
+  final _endTimeController = TextEditingController();
+  final _endDateController = TextEditingController();
   DateTime start = DateTime.now();
+  late int duration;
+  late DateTime end;
 
   @override
   void initState() {
@@ -26,8 +31,14 @@ class _CreateEventPageState extends State<CreateEventPage> {
     DateTime start = widget.timeSlot == null
         ? DateTime.now()
         : getNextDateTimeFromTimeSlot(DateTime.now(), widget.timeSlot!);
-    _startDateController.text = DateFormat.yMd().format(start);
+    duration = widget.group.meetingDuration == null
+        ? 2
+        : widget.group.meetingDuration!.toInt();
+    end = start.add(Duration(hours: duration));
+    _startDateController.text = DateFormat.yMMMMEEEEd().format(start);
     _startTimeController.text = DateFormat.jm().format(start);
+    _endDateController.text = DateFormat.yMMMMEEEEd().format(end);
+    _endTimeController.text = DateFormat.jm().format(end);
   }
 
   @override
@@ -62,49 +73,95 @@ class _CreateEventPageState extends State<CreateEventPage> {
               ),
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-                controller: _startDateController,
-                decoration: InputDecoration(
-                  labelText: 'Date',
-                  filled: true,
-                  enabledBorder:
-                      OutlineInputBorder(borderSide: BorderSide.none),
-                  prefixIcon: Icon(Icons.calendar_today),
+          Row(
+            children: [
+              SizedBox(
+                width: 300,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                      controller: _startDateController,
+                      decoration: InputDecoration(
+                        labelText: 'Start Date',
+                        filled: true,
+                        enabledBorder:
+                            OutlineInputBorder(borderSide: BorderSide.none),
+                        prefixIcon: Icon(Icons.calendar_today),
+                      ),
+                      readOnly: true,
+                      onTap: () {
+                        _selectStartDate(context, start);
+                      }),
                 ),
-                readOnly: true,
-                onTap: () {
-                  _selectDate(context, start);
-                }),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-                controller: _startTimeController,
-                decoration: InputDecoration(
-                  labelText: 'Start Time',
-                  filled: true,
-                  enabledBorder:
-                      OutlineInputBorder(borderSide: BorderSide.none),
-                  prefixIcon: Icon(Icons.schedule),
+              ),
+              SizedBox(
+                width: 150,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                      controller: _startTimeController,
+                      decoration: InputDecoration(
+                        labelText: 'Start Time',
+                        filled: true,
+                        enabledBorder:
+                            OutlineInputBorder(borderSide: BorderSide.none),
+                        prefixIcon: Icon(Icons.schedule),
+                      ),
+                      readOnly: true,
+                      onTap: () {
+                        _selectStartTime(
+                            context, TimeOfDay.fromDateTime(start));
+                      }),
                 ),
-                readOnly: true,
-                onTap: () {
-                  _selectTime(context, TimeOfDay.fromDateTime(start));
-                }),
+              ),
+            ],
           ),
+
           //TODO: set up end time to default to group meeting duration + start time
           //TODO: set up end time to not allow before start time
           //TODO: set up end time to move when a new start is selected
           //TODO: set up an end date field for events that span midnight
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              decoration: InputDecoration(
-                labelText: 'End Time',
+          Row(
+            children: [
+              SizedBox(
+                width: 300,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                      controller: _endDateController,
+                      decoration: InputDecoration(
+                        labelText: 'End Date',
+                        filled: true,
+                        enabledBorder:
+                            OutlineInputBorder(borderSide: BorderSide.none),
+                        prefixIcon: Icon(Icons.calendar_today),
+                      ),
+                      readOnly: true,
+                      onTap: () {
+                        _selectEndDate(context, end);
+                      }),
+                ),
               ),
-            ),
+              SizedBox(
+                width: 150,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                      controller: _endTimeController,
+                      decoration: InputDecoration(
+                        labelText: 'End Time',
+                        filled: true,
+                        enabledBorder:
+                            OutlineInputBorder(borderSide: BorderSide.none),
+                        prefixIcon: Icon(Icons.schedule),
+                      ),
+                      readOnly: true,
+                      onTap: () {
+                        _selectEndTime(context, TimeOfDay.fromDateTime(end));
+                      }),
+                ),
+              ),
+            ],
           ),
           Padding(
               padding: const EdgeInsets.all(8.0),
@@ -113,7 +170,7 @@ class _CreateEventPageState extends State<CreateEventPage> {
         ]));
   }
 
-  Future<void> _selectDate(BuildContext context, DateTime start) async {
+  Future<void> _selectStartDate(BuildContext context, DateTime start) async {
     final DateTime? picked = await showDatePicker(
       context: context,
       initialDate: start,
@@ -126,7 +183,7 @@ class _CreateEventPageState extends State<CreateEventPage> {
       });
   }
 
-  Future<void> _selectTime(BuildContext context, TimeOfDay start) async {
+  Future<void> _selectStartTime(BuildContext context, TimeOfDay start) async {
     final TimeOfDay? picked = await showTimePicker(
       context: context,
       initialTime: start,
@@ -135,6 +192,31 @@ class _CreateEventPageState extends State<CreateEventPage> {
     if (picked != null)
       setState(() {
         _startTimeController.text = picked.toString();
+      });
+  }
+
+  Future<void> _selectEndDate(BuildContext context, DateTime end) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: end,
+      firstDate: DateTime.now(),
+      lastDate: DateTime(2050),
+    );
+    if (picked != null)
+      setState(() {
+        _endDateController.text = picked.toString().split(" ")[0];
+      });
+  }
+
+  Future<void> _selectEndTime(BuildContext context, TimeOfDay end) async {
+    final TimeOfDay? picked = await showTimePicker(
+      context: context,
+      initialTime: end,
+    );
+
+    if (picked != null)
+      setState(() {
+        _endTimeController.text = picked.toString();
       });
   }
 }
