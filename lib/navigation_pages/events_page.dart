c5ab87db1738404314fc6133f661d8697d50ae95
Avery Pustina <avepus@gmail.com>
2024-05-16 22:14:51 -0500

    comment next todo

--- lib/navigation_pages/events_page.dart
+++ lib/navigation_pages/events_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class EventsPage extends StatelessWidget {
   const EventsPage({super.key});
 
+  //TODO: next up: implement events page
   @override
   Widget build(BuildContext context) {
     return Scaffold(
