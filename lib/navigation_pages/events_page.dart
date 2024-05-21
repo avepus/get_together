212203ebc9124b942ec4e7e3a923864751ac87d6
avepus <avepus@gmail.com>
2024-05-20 21:02:10 -0500

    progress on create event button

--- lib/navigation_pages/events_page.dart
+++ lib/navigation_pages/events_page.dart
@@ -10,6 +10,7 @@ import '../classes/group.dart';
 import '../classes/event.dart';
 import '../widgets/image_with_null_error_handling.dart';
 import '../utils.dart';
+import '../update_event.dart';
 
 class EventsPage extends StatefulWidget {
   const EventsPage({super.key});
@@ -25,37 +26,58 @@ class _EventsPageState extends State<EventsPage> {
       builder: (context) {
         return AlertDialog(
           title: const Text('Select Group'),
-          content: FutureBuilder<QuerySnapshot>(
-            future: FirebaseFirestore.instance.collection(Group.collectionName).where(Group.adminsKey, arrayContains: appState.loginUserDocumentId).get(),
-            builder: (context, snapshot) {
-              if (snapshot.hasError) {
-                return Text('Error: ${snapshot.error}');
-              }
+          content: Container(
+            height: 500,
+            width: 300,
+            child: FutureBuilder<QuerySnapshot>(
+              future: FirebaseFirestore.instance.collection(Group.collectionName).where(Group.adminsKey, arrayContains: appState.loginUserDocumentId).get(),
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  return Text('Error: ${snapshot.error}');
+                }
 
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const CircularProgressIndicator();
-              }
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const CircularProgressIndicator();
+                }
 
-              if (!snapshot.hasData) {
-                return const Text('No data');
-              }
+                if (!snapshot.hasData) {
+                  return const Text('No data');
+                }
 
-              return ListView.builder(
-                itemCount: snapshot.data!.docs.length,
-                itemBuilder: (context, index) {
-                  var group = Group.fromDocumentSnapshot(snapshot.data!.docs[index]);
-                  //this code relies on knowing the group structure. would be better if it didn't
-                  //I tried to extract this as group method to return the ListTile, but I couldn't get the navigfation to work
-                  return ListTile(
-                      leading: ImageWithNullAndErrorHandling(imageUrl: group.imageUrl),
-                      title: Text(group.name ?? '<No Name>'),
-                      subtitle: group.description != null ? Text(group.description!) : null,
-                      onTap: () {
-                        //TODO: next up: Implement this to create a new event
-                      });
-                },
-              );
-            },
+                return ListView.builder(
+                  itemCount: snapshot.data!.docs.length,
+                  itemBuilder: (context, index) {
+                    Group group = Group.fromDocumentSnapshot(snapshot.data!.docs[index]);
+                    //this code relies on knowing the group structure. would be better if it didn't
+                    //I tried to extract this as group method to return the ListTile, but I couldn't get the navigfation to work
+                    return ListTile(
+                        leading: ImageWithNullAndErrorHandling(imageUrl: group.imageUrl),
+                        title: Text(
+                          group.name ?? '<No Name>',
+                          maxLines: 1,
+                        ),
+                        subtitle: group.description != null
+                            ? Text(
+                                group.description!,
+                                maxLines: 1,
+                              )
+                            : null,
+                        onTap: () {
+                          context.pop();
+                          showDialog(
+                              context: context,
+                              builder: (context) {
+                                return AlertDialog(
+                                  title: const Text('Select Timeslot'),
+                                  //TODO: get rid of magic number 5 below
+                                  content: GenerateEventButton(group: group, timeSlotDuration: group.meetingDurationTimeSlots, numberOfSlotsToReturn: 5),
+                                );
+                              });
+                        });
+                  },
+                );
+              },
+            ),
           ),
         );
       },
