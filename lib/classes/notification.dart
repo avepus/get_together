a1b89168d6df86100acb01c51ac3343e2349e71f
avepus <avepus@gmail.com>
2024-06-06 21:09:10 -0500

    progress on notification class

--- lib/classes/notification.dart
+++ lib/classes/notification.dart
@@ -1,48 +1,49 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-
 ///current plan for notifications is to have them stored in an array of Maps in the user document
+///an instance of NOtification represents a single node in that array
+
+enum NotificationTypes {
+  friendRequest,
+}
 
 class Notification {
-  static const String collectionName = 'notifications';
   static const String titleKey = 'title';
   static const String descriptionKey = 'description';
   static const String typeKey = 'type';
-  static const String routeKey = 'route';
 
-  String documentId = '';
   String title;
   String description;
-  String type;
-  String route;
+  int type;
 
   Notification({
     required this.title,
     required this.description,
     required this.type,
-    required this.route,
   });
+
+  //creates a notification from a single
+  factory Notification.fromNotificationArray(Map<String, String> notificationMap) {
+    _retreivedFirestoreDataAssertions(notificationMap);
+    return Notification(
+      title: notificationMap[titleKey]!,
+      description: notificationMap[descriptionKey]!,
+      type: int.tryParse(notificationMap[typeKey]!)!,
+    );
+  }
+
+  ///This method is used to ensure that the data retrieved from firestore is in the correct format
+  static void _retreivedFirestoreDataAssertions(Map<String, String> notificationMap) {
+    assert(notificationMap.containsKey(titleKey), 'Notification map does not contain title key which is required and should never be missing');
+    assert(notificationMap.containsKey(descriptionKey), 'Notification map does not contain description key which is required and should never be missing');
+    assert(notificationMap.containsKey(typeKey), 'Notification map does not contain type key which is required and should never be missing');
+    int? typeValue = int.tryParse(notificationMap[typeKey]!);
+    assert(typeValue != null, 'Notification type value is not an integer');
+  }
 
   Map<String, dynamic> toMap() {
     return {
       titleKey: title,
       descriptionKey: description,
       typeKey: type,
-      routeKey: route,
     };
-  }
-
-  Future<void> saveToFirestore() async {
-    if (documentId != '') {
-      await FirebaseFirestore.instance.collection(collectionName).doc(documentId).set(toMap());
-    } else {
-      DocumentReference ref = await FirebaseFirestore.instance.collection(collectionName).add(toMap());
-      documentId = ref.id;
-    }
-  }
-
-  void deleteFromFirestore() {
-    if (documentId != '') {
-      FirebaseFirestore.instance.collection(collectionName).doc(documentId).delete();
-    }
   }
 }
