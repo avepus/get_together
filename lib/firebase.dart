bccbf46ad7a79fd02c89d5317371cd8b5ca9c5e2
avepus <avepus@gmail.com>
2025-06-29 22:53:46 -0500

    update to use enumToIndexNameString

--- lib/firebase.dart
+++ lib/firebase.dart
@@ -8,6 +8,7 @@ import 'package:rxdart/rxdart.dart';
 import 'classes/group.dart';
 import 'classes/app_notification.dart';
 import 'classes/event.dart';
+import 'utils.dart';
 
 Future<List<AppUser>> fetchAllUsers() async {
   CollectionReference users = FirebaseFirestore.instance.collection('users');
@@ -187,7 +188,7 @@ Future<void> markEventAsCancelled(Event event, Group group) async {
   assert(event.documentId != null, 'Event document ID is null but it never should be when we are attempting to cancel');
   CollectionReference events = FirebaseFirestore.instance.collection(Event.collectionName);
   events.doc(event.documentId).update({
-    Event.statusKey: EventStatus.cancelled.index,
+    Event.statusKey: enumToIndexNameString(EventStatus.cancelled),
   });
 }
 
